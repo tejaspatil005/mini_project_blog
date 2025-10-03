d15a94734357a98c83675cfb7c34a9f32efa4bd8
tejaspatil005 <106586060+tejaspatil005@users.noreply.github.com>
2025-10-03 22:31:41 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+post=[
+    {
+        'author':'Tejas patil',
+        'title':'Blog Post 1',
+        'content':'first post content',
+        'date_posted':'August 1, 2025'
+    },
+    {
+        'author':'riya patil',
+        'title':'blog post 2',
+        'content':'second post content',
+        'date_posted':'oct 2 ,2025'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Keep moving Forward......</h1>')
+    context={
+        'posts':post
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1> this is about to end....!!!</h1>')
+    return render(request,'blog/about.html',{'title':'about'})
