67c1d0983397e6f83d7ba58141beb22e368b6b45
tejaspatil005 <106586060+tejaspatil005@users.noreply.github.com>
2025-10-09 23:04:08 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import post
 
-post=[
-    {
-        'author':'Tejas patil',
-        'title':'Blog Post 1',
-        'content':'first post content',
-        'date_posted':'August 1, 2025'
-    },
-    {
-        'author':'riya patil',
-        'title':'blog post 2',
-        'content':'second post content',
-        'date_posted':'oct 2 ,2025'
-    }
-]
 def home(request):
     context={
-        'posts':post
+        'posts':post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
