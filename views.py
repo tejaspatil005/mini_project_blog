801714ef858d2bb1dd97caa786e14d41828dc0c4
tejaspatil005 <106586060+tejaspatil005@users.noreply.github.com>
2025-10-15 22:50:03 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from .form import UserRegisterForm
+from django.contrib.auth import logout
 
 
 def register(request):
     if request.method=='POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username=form.cleaned_data.get('Username')
-            messages.success(request,f'Account created for {username}')
-            return redirect('blog-home')
-        else:
-          form=UserCreationForm()
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Your account has been created !')
+            return redirect('login')    
+    else:
+           form=UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
+
+# Custom logout view (works for GET requests)
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
+
+@login_required
+def profile(request):
+     return render(request,'users/profile.html')
